239c41766a0612c52ad73e9a87def27beb09bd8e
Peter Grassberger <p.grassberger@student.tugraz.at>
2018-07-02 17:59:44 +0200

    set csv parameters as optional parameters of timesplit_csv

--- timesplit-csv.py
+++ timesplit-csv.py
@@ -3,17 +3,13 @@ import os.path
 import pandas as pd
 
 
-def timesplit_csv(input, output='.'):
+def timesplit_csv(input, output='.', csv_date_format='%d.%m.%Y', csv_delimiter=';', csv_quotechar='"', csv_encoding='ISO/-8859-1'):
     if not os.path.isfile(input):
         print('Error: File "{0}" don\'t exist.'.format(input))
         return
     if not os.path.exists(output):
         print('Error: No such directory "{0}".'.format(output))
         return
-    csv_date_format = '%d.%m.%Y'
-    csv_delimiter = ';'
-    csv_quotechar = '"'
-    csv_encoding = 'ISO/-8859-1'
     date_parser = lambda x: pd.datetime.strptime(x, csv_date_format)
     amount_parser = lambda x: float(x.replace('.', '').replace(',', '.'))
     data = pd.read_csv(filepath_or_buffer=input, delimiter=csv_delimiter, quotechar=csv_quotechar, encoding=csv_encoding, header=None,
